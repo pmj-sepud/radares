c2b4b5140c7ef7903ee837350e0ebfaa8f99fdc1
Rodrigo Xavier <rodrigoxavierfelippe@gmail.com>
2018-07-20 08:08:39 -0300

    Amend bash

--- cron/script.bash
+++ cron/script.bash
@@ -1,4 +1,3 @@
 #!/usr/bin/python
 source activate radars
-/home/sepud/projetos/radares/src/scrape.py | awk '{ print strftime("%c: "), $0; fflush(); }' > /home/sepud/projetos/radares/log/output.log
-# 35 17 * * * bash /home/sepud/projetos/radares/cron/script.bash
+python /home/sepud/projetos/radares/src/scrape.py | awk '{ print strftime("%c: "), $0; fflush(); }' > /home/sepud/projetos/radares/log/output.log
