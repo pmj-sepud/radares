3aa14d8f8cf2926dcf0c7de1577837b19e3a86b6
Rodrigo Xavier <rodrigoxavierfelippe@gmail.com>
2018-07-20 08:04:33 -0300

    include commands in bash to run as cron

--- cron/script.bash
+++ cron/script.bash
@@ -1,2 +1,4 @@
+#!/usr/bin/python
 source activate radars
 /home/sepud/projetos/radares/src/scrape.py | awk '{ print strftime("%c: "), $0; fflush(); }' > /home/sepud/projetos/radares/log/output.log
+# 35 17 * * * bash /home/sepud/projetos/radares/cron/script.bash
